5378fac0c136c1765abb8e13a6d3ca23f05fb639
sjz123321 <shijiezuan@gmail.com>
2018-08-26 13:06:16 +0800

    Signed-off-by: sjz123321 <shijiezuan@gmail.com>

--- run_goproxy.sh
+++ run_goproxy.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
-ip_addr=`curl ifconfig.me`
+touch /etc/my_ip_addr.conf
+ip_temp=`sed -n "1p" /etc/my_ip_addr.conf`
+if [ `cat /etc/my_ip_addr.conf | wc -l` -eq "0" ] ; then
+     echo "*****************************************"
+     echo "   您的VPS外网ip未知，请选择获取方式"
+	 echo "1.自动获取（如失败，请手动） 2.手动输入ip"
+	 echo "*****************************************"
+	 read temp_first
+	 if [ $temp_first -eq "1" ] ; then
+	     echo "正在获取vps外网ip"
+	     ip_addr=`curl ifconfig.me`
+		 echo $ip_addr >> /etc/my_ip_addr.conf
+		 echo "获取vps外网ip成功 ip是 $ip_addr"
+		 echo "如果设置错误请删除/etc/my_ip_addr 使用rm -f /etc/my_ip_addr.conf 指令"
+	 elif [ $temp_first -eq "2" ] ; then
+	     echo "请输入外网ip"
+	     read ip_addr
+		 echo $ip_addr >> /etc/my_ip_addr.conf
+		 echo "设置vps外网ip成功 ip是 $ip_addr"
+		 echo "如果设置错误请删除/etc/my_ip_addr 使用rm -f /etc/my_ip_addr.conf 指令"
+	 fi
+else
+     ip_addr=$ip_temp
+	 echo "您的vps外网ip为 $ip_addr"
+fi
+
+
 secure_para="-g $ip_addr"
 bk="--daemon"
 guard="--forever"
@@ -23,6 +49,7 @@ echo "      3.打开socks5代理"
 echo "      4.打开tcp代理"
 echo "      5.打开udp代理"
 echo "      6.删除所有proxy开机任务"
+echo "      7.停止所有后台proxy服务"
 echo "***************************************"
 read choice
 case $choice in
@@ -88,6 +115,9 @@ case $choice in
          log_path_temp=$NULL
          log_para_temp=$NULL
 		 cd $path
+		 echo "请在本地打开服务指令为："
+		 echo "proxy http -t tcp -p ":8080" -T tls -P "$ip_addr:$port" -C proxy.crt -K proxy.key "
+		 echo "#默认本地端口为8080 请将证书文件和可执行文件放置在同一目录下"
 		 run_proxy "http $secure_para_temp -t tls -p "":$port"" -C $path/proxy.crt -K $path/proxy.key $guard_temp $log_para_temp $bk_temp"
 		 exit 
      fi
@@ -118,6 +148,9 @@ case $choice in
          echo "proxy "http $secure_para_temp -t tls -p "":$port"" -C $path/proxy.crt -K $path/proxy.key $guard_temp $log_para_temp $bk_temp" " >> /etc/auto_run_proxy.sh
      fi
 	 cd $path
+	 echo "请在本地打开服务指令为："
+	 echo "proxy http -t tcp -p ":8080" -T tls -P "$ip_addr:$port" -C proxy.crt -K proxy.key "
+	 echo "#默认本地端口为8080 请将证书文件和可执行文件放置在同一目录下"
 	 run_proxy "http $secure_para_temp -t tls -p "":$port"" -C $path/proxy.crt -K $path/proxy.key $guard_temp $log_para_temp $bk_temp"
    elif [ $temp -eq "2" ] ; then
      echo "未完待续"
@@ -208,6 +241,9 @@ case $choice in
          log_path_temp=$NULL
          log_para_temp=$NULL
 		 cd $path
+		 echo "请在本地打开服务指令为："
+		 echo "proxy http -p "0.0.0.0:8080" -T tcp -P "$ip_addr:$port""
+	     echo "#默认本地端口为8080 "
 		 run_proxy "http $secure_para_temp -t tcp -p "":$port"" $guard_temp $log_para_temp $bk_temp"
 		 exit 
      fi
@@ -238,6 +274,9 @@ case $choice in
          echo "proxy "http $secure_para_temp -t tcp -p ""0.0.0.0:$port"" $guard_temp $log_para_temp $bk_temp" " >> /etc/auto_run_proxy.sh
      fi
 	 cd $path
+	 echo "请在本地打开服务指令为："
+	 echo "proxy http -p "0.0.0.0:8080" -T tcp -P "$ip_addr:$port""
+	 echo "#默认本地端口为8080 "
 	 run_proxy "http $secure_para_temp -t tcp -p ""0.0.0.0:$port"" $guard_temp $log_para_temp $bk_temp"
    elif [ $temp -eq "5" ] ; then
      echo "未完待续"
@@ -270,6 +309,9 @@ case $choice in
          log_path_temp=$NULL
          log_para_temp=$NULL
 		 cd $path
+		 echo "请在本地打开服务指令为："
+	     echo "proxy http -t tcp -p ":8080" -T kcp -P "$ip_addr:$port" --kcp-key $passwd"
+	     echo "#默认本地端口为8080 "
 		 run_proxy "http $secure_para_temp -t kcp -p ":$port" --kcp-key $passwd $guard_temp $log_para_temp $bk_temp"
 		 exit 
      fi
@@ -299,6 +341,9 @@ case $choice in
 	 if [ $tempp -eq "1" ] ; then
          echo "proxy "http $secure_para_temp -t kcp -p ":$port" --kcp-key $passwd $guard_temp $log_para_temp $bk_temp" " >> /etc/auto_run_proxy.sh
      fi
+   echo "请在本地打开服务指令为："
+   echo "proxy http -t tcp -p ":8080" -T kcp -P "$ip_addr:$port" --kcp-key $passwd"
+   echo "#默认本地端口为8080 "
    run_proxy "http $secure_para_temp -t kcp -p ":$port" --kcp-key $passwd $guard_temp $log_para_temp $bk_temp"
 
    ;;
@@ -353,6 +398,9 @@ case $choice in
          log_path_temp=$NULL
          log_para_temp=$NULL
 		 cd $path
+		 echo "请在本地打开服务指令为："
+		 echo "proxy socks -t tcp -p ":8080" -T tls -P "$ip_addr:$port" -C proxy.crt -K proxy.key "
+		 echo "#默认本地端口为8080 请将证书文件和可执行文件放置在同一目录下"
 		 run_proxy "socks $secure_para_temp -t tls -p "":$port"" -C $path/proxy.crt -K $path/proxy.key $guard_temp $log_para_temp $bk_temp"
 		 exit 
      fi
@@ -376,6 +424,9 @@ case $choice in
 	     log_para_temp=""
 	 fi
 	 cd $path
+	 echo "请在本地打开服务指令为："
+	 echo "proxy socks -t tcp -p ":8080" -T tls -P "$ip_addr:$port" -C proxy.crt -K proxy.key "
+	 echo "#默认本地端口为8080 请将证书文件和可执行文件放置在同一目录下"
 	 run_proxy "socks $secure_para_temp -t tls -p "":$port"" -C $path/proxy.crt -K $path/proxy.key $guard_temp $log_para_temp $bk_temp"
    elif [ $temp -eq "2" ] ; then
      echo "未完待续"
@@ -413,6 +464,9 @@ case $choice in
          log_path_temp=$NULL
          log_para_temp=$NULL
 		 cd $path
+		 echo "请在本地打开服务指令为："
+		 echo "proxy socks -p "0.0.0.0:8080" -T tcp -P "$ip_addr:$port""
+	     echo "#默认本地端口为8080 "
 		 run_proxy "socks $secure_para_temp -t tcp -p "":$port"" $guard_temp $log_para_temp $bk_temp"
 		 exit 
      fi
@@ -436,6 +490,9 @@ case $choice in
 	     log_para_temp=""
 	 fi
 	 cd $path
+	 echo "请在本地打开服务指令为："
+	 echo "proxy socks -p "0.0.0.0:8080" -T tcp -P "$ip_addr:$port""
+	 echo "#默认本地端口为8080 "
 	 run_proxy "socks $secure_para_temp -t tcp -p ""0.0.0.0:$port"" $guard_temp $log_para_temp $bk_temp"
    elif [ $temp -eq "4" ] ; then
      echo "***************************************"
@@ -459,6 +516,9 @@ case $choice in
          log_path_temp=$NULL
          log_para_temp=$NULL
 		 cd $path
+		 echo "请在本地打开服务指令为："
+		 echo "proxy socks -p "0.0.0.0:8080" -T tcp -P "$ip_addr:$port""
+	     echo "#默认本地端口为8080 "
 		 run_proxy "socks $secure_para_temp -t tcp -p "":$port"" $guard_temp $log_para_temp $bk_temp"
 		 exit 
      fi
@@ -482,6 +542,9 @@ case $choice in
 	     log_para_temp=""
 	 fi
 	 cd $path
+	 echo "请在本地打开服务指令为："
+	 echo "proxy socks -p "0.0.0.0:8080" -T tcp -P "$ip_addr:$port""
+	 echo "#默认本地端口为8080 "
 	 run_proxy "socks $secure_para_temp -t tcp -p ""0.0.0.0:$port"" $guard_temp $log_para_temp $bk_temp"
    elif [ $temp -eq "5" ] ; then
      echo "未完待续"
@@ -503,6 +566,17 @@ case $choice in
    touch /etc/auto_run_proxy.sh
    chmod +x /etc/auto_run_proxy.sh
    echo "删除proxy开机任务成功"
+   
+   ;;
+   
+   
+7) echo "正在关闭所有proxy后台进程"
+   pkill proxy
+   echo "proxy进程清理完毕"
+   
+   ;;
+   
+   
 esac
 
 
