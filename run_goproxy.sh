3a3c0092aa1fea421725313b39f35fb937ae5360
sjz123321 <shijiezuan@gmail.com>
2018-08-28 16:37:00 +0800

    Signed-off-by: sjz123321 <shijiezuan@gmail.com>

--- run_goproxy.sh
+++ run_goproxy.sh
@@ -42,7 +42,7 @@ function run_proxy()
 
 echo "        欢迎使用goproxy配置脚本        "
 echo "***************************************"
-echo "      0.安装/更新goproxy（仅64位系统）"
+echo "      0.安装/更新goproxy及脚本（仅64位系统）"
 echo "      1.打开http-tcp代理"
 echo "      2.打开http-kcp代理"
 echo "      3.打开socks5代理"
@@ -50,12 +50,16 @@ echo "      4.打开tcp代理"
 echo "      5.打开udp代理"
 echo "      6.删除所有proxy开机任务"
 echo "      7.停止所有后台proxy服务"
+echo "      8.卸载goproxy脚本及goproxy"
 echo "***************************************"
 read choice
 case $choice in
 
 0) curl -L https://raw.githubusercontent.com/snail007/goproxy/master/install_auto.sh | bash
-   cp proxy.service /etc 
+   rm -rf /root/goproxy-shell/
+   git clone https://github.com/sjz123321/goproxy-shell.git /root/goproxy-shell/
+   rm -f /usr/bin/run_goproxy.sh
+   cp /root/goproxy-shell/run_goproxy.sh /usr/bin
    touch /etc/auto_run_proxy.sh
    chmod +x /etc/auto_run_proxy.sh
    echo "proxy 安装成功"
@@ -356,7 +360,7 @@ case $choice in
    echo "***********请选择代理类型*************"
    echo "1.使用TLS加密（二级代理）2.使用TLS加密（多(>=3)级代理）"
    echo "3.不使用TLS加密（一级代理）4.不使用TLS加密（二级代理）"
-   echo "5.不使用TLS加密（多（>=3）级代理）"
+   echo "5.不使用TLS加密（多（>=3）级代理）6.使用kcp传输协议"
    echo "***************************************"
    read temp
    if [ $temp -eq "1" ] ; then
@@ -548,9 +552,64 @@ case $choice in
 	 run_proxy "socks $secure_para_temp -t tcp -p ""0.0.0.0:$port"" $guard_temp $log_para_temp $bk_temp"
    elif [ $temp -eq "5" ] ; then
      echo "未完待续"
-   fi
+   elif [ $temp -eq "6" ] ; then
+     echo "***************************************"
+     echo "     请输入需要使用的服务器端口号      "
+     echo "***************************************"
+     read port
+     echo "***************************************"
+     echo "          请设置kcp传输密码            "
+     echo "***************************************"
+     read passwd
+     echo "是否监控proxy运行 （默认为后台，不监控）"
+     echo "1.否 2.是"
+     read -t 15 tempp
+	 tempp=${tempp:-1}
+	 if [ $tempp -eq "2" ] ; then
+         bk_temp=$NULL
+         guard_temp=$NULL
+         log_path_temp=$NULL
+         log_para_temp=$NULL
+		 cd $path
+		 echo "请在本地打开服务指令为："
+	     echo "proxy http -t tcp -p ":8080" -T kcp -P "$ip_addr:$port" --kcp-key $passwd"
+	     echo "#默认本地端口为8080 "
+		 run_proxy "http $secure_para_temp -t kcp -p ":$port" --kcp-key $passwd $guard_temp $log_para_temp $bk_temp"
+		 exit 
+     fi
+     echo "是否守护执行 （默认为使用）"
+     echo "1.是 2.否"
+     read -t 15 tempp
+     tempp=${tempp:-1}
+     if [ $tempp -eq "2" ] ; then
+           guard_temp=$NULL
+     fi
+     echo "日志功能 （默认为开启 存储地址为$log_path）"
+     echo "1.是(默认地址) 2.是（自定义存储地址） 3.否"
+     read -t 15 tempp
+     tempp=${tempp:-1}
+     if [ $tempp -eq "2" ] ; then
+         echo "请指定log存储地址（请确定地址有效！否则请在使用前先mkdir地址）"
+		 read path_temp
+		 log_path_temp=$path_temp
+		 log_para_temp="--log $log_path_temp"
+     elif [ $tempp -eq "3" ] ; then
+	     log_para_temp=""
+     fi
+     echo "是否加入开机自启动 (默认为否)"
+     echo "1.是 2.否"
+     read -t 15 tempp
+     tempp=${tempp:-2}
+	 if [ $tempp -eq "1" ] ; then
+         echo "proxy "socks $secure_para_temp -t kcp -p ":$port" --kcp-key $passwd $guard_temp $log_para_temp $bk_temp" " >> /etc/auto_run_proxy.sh
+     fi
+     echo "请在本地打开服务指令为："
+     echo "proxy socks -t tcp -p ":8080" -T kcp -P "$ip_addr:$port" --kcp-key $passwd"
+     echo "#默认本地端口为8080 "
+     run_proxy "socks $secure_para_temp -t kcp -p ":$port" --kcp-key $passwd $guard_temp $log_para_temp $bk_temp"
+     fi
    
-
+   
    ;;
    
 4) echo "未完待续"
@@ -576,7 +635,12 @@ case $choice in
    
    ;;
    
-   
+8) rm -f /etc/proxy/blocked
+   rm -f /etc/proxy/direct
+   rm -f /usr/bin/proxy
+   rm -rf /root/goproxy-shell/
+   rm -f /usr/bin/run_goproxy.sh
+   echo "卸载完成，谢谢使用，再见！"
 esac
 
 
